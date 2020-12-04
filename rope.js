28d3acbb67c4daeca10ba1a0169855a4ae9acbfc
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-04 21:28:34 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -1,9 +1,9 @@
-class sling{
+class rope{
     constructor(bodyA, point2){
         var options = {
             bodyA: bodyA,
             pointB: point2,
-            stiffness: 0.05,
+            stiffness: 3,
             length: 10
         }
         this.sling = Constraint.create(options);
@@ -11,17 +11,16 @@ class sling{
 
         this.posB=point2
     }
-    
+
     move(){
         this.sling.bodyA=null;
     }
 
     display(){
-
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.posB;
-            strokeWeight(4);
+            strokeWeight(3);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
       }
     }
