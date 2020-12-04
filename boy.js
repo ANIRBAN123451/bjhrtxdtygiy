e6fc7d3213f022f234ddb4161ed6cf4658977be5
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-04 14:19:12 +0530

    Update boy.js

--- boy.js
+++ boy.js
@@ -1,26 +1,17 @@
 class character{
-   constructor(x,y,width,height){
+    constructor(x, y, width, height) {
+        this.body = Bodies.rectangle(x, y, width, height);
+        this.image = loadImage("boy.png");
+        World.add(world, this.body);
 
-        this.image=loadImage("boy.png")
-        this.image.scale=0.03;
-
-        this.h=height
-        this.w=width
-        this.x=x
-        this.y=y
-
-   }
-
-   display(){
-       push();
-       imageMode(CENTER)
-       image(this.image,this.x,this.y,this.w,this.h)
-
-       pop();
-   }
-
-
-
-
-
+        this.width = width;
+        this.height = height;
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
 }
