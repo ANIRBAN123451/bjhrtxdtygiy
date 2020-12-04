5830930124ece2268f1f1ec2a3d7c1f90f3df773
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-04 14:22:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,9 +23,9 @@ function setup() {
 	cr1=new character(200,340,100,200);
 	ob1=new object(160,293,27,27);
 	tree1=new obstacle(1000,370,300,400)
-	food0=new fruit(1090,130,50,50)
-	food1=new fruit(990,80,50,50)
-	food2=new fruit(920,140,50,50)
+	food0=new fruit(1090,130,50,50,{isStatic:true})
+	food1=new fruit(990,80,50,50,{isStatic:true})
+	food2=new fruit(920,140,50,50,{isStatic:true})
 	
 
 	rope1=new new sling(ob1.body,{x:160,y:293})
