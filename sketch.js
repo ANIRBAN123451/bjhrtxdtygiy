bfc2eecd8d26b5337ea5d601a05f3f323ab02d37
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-06 21:35:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
     ground1=new ground(600,395,1200,10);
 	boy=new character(200,340,100,200);
 	stone1=new weapon(160,293,27,27);
-	tree1=new tree(1000,370,300,400)
+	tree1=new tree(1000,300,300,400)
 	mango1=new fruit(1090,130,50,50)
 	mango2=new fruit(990,80,50,50)
 	mango3=new fruit(920,140,50,50)
@@ -33,10 +33,9 @@ function draw() {
   background(bgImage);
   Engine.update(engine);
 
-  /*detectCollision(ob1,food0);
-  detectCollision(ob1,food1);
-  detectCollision(ob1,food2);
-  */
+  detectCollision(stone,mango1);
+  detectCollision(stone,mango2);
+  detectCollision(stone,mango3);
 
  fill("green")
   ground1.show();
