9a596468b10f1d3742fc7f0daec85550a08d734d
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-06 22:27:49 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,7 +1,7 @@
 class tree{
     constructor(x, y, width, height) {
       var op={
-        isStatic:true
+        isStatic:false
     }
         this.body = Bodies.rectangle(x, y, width, height,op);
         this.image = loadImage("tree.png");
