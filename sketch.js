a5947b6cd530f91edcec493fe3000fcc73f5ee4a
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-06 19:37:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint=Matter.Constraint;
 
-var wall1;
+
 
 function preload(){
 	bgImage=loadImage("bg.jpg")
@@ -19,16 +19,16 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-    wall1=new ground(600,395,1200,10);
-	cr1=new character(200,340,100,200);
-	ob1=new object(160,293,27,27);
-	tree1=new obstacle(1000,370,300,400)
-	food0=new fruit(1090,130,50,50)
-	food1=new fruit(990,80,50,50)
-	food2=new fruit(920,140,50,50)
+        ground1=new ground(600,395,1200,10);
+	boy=new character(200,340,100,200);
+	stone=new weapon(160,293,27,27);
+	tree1=new tree(1000,370,300,400)
+	mango1=new fruit(1090,130,50,50)
+	mango2=new fruit(990,80,50,50)
+	mango3=new fruit(920,140,50,50)
 	
 
-	rope1=new rope(ob1.body,{x:160,y:293})
+	rope1=new rope(boy.body,{x:160,y:293})
 	
 }
 
@@ -44,17 +44,17 @@ function draw() {
  fill("green")
   wall1.show();
 
-  cr1.display();
-  ob1.display();
+  boy.display();
+  stone.display();
   tree1.display();
-  food0.display();
-  food1.display();
-  food2.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
   rope1.display();
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(ob1.body,{x:mouseX,y:mouseY});
+    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
 }
 
 function mouseReleased(){
