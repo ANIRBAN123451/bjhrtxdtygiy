c305858273b9e8099465ebad52a68ad2fad0a038
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-07 20:54:26 +0530

    Update mangoes.js

--- mangoes.js
+++ mangoes.js
@@ -1,22 +1,23 @@
 class fruit{
-    constructor(x, y, width, height) {
-        var op={
-        isStatic:true,
-        restitution:0,
-        friction:1
+  constructor(x,y,radius){
+      var op={
+          isStatic: false,
+          restitution: 0,
+          friction: 1
       }
-        this.body = Bodies.rectangle(x, y, width, height,op);
-        this.image = loadImage("mango.png");
-        World.add(world, this.body);
 
-        this.width = width;
-        this.height = height;
-      }
-      display(){
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
-      }
+      this.body=Bodies.circle(x,y,radius/2,op);
+      this.radius= radius;
+      this.image=loadImage("mango.png");
+      World.add(world,this.body);
+  }
+
+  display(){
+      push(); 
+      translate(this.body.position.x, this.body.position.y,this.radius);
+      
+      image( this.image, 0,0,50,50) ; 
+      rotate(this.body.angle); 
+      pop();
+  }
 }
