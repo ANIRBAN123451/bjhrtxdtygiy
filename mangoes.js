2718a4966c25a2c5feb202d69b1ff9d3fd3cc9f2
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-07 21:26:35 +0530

    Update mangoes.js

--- mangoes.js
+++ mangoes.js
@@ -15,7 +15,7 @@ class fruit{
   display(){
       push(); 
       translate(this.body.position.x, this.body.position.y,this.radius);
-      
+      imageMode(CENTER);
       image( this.image, 0,0,50,50) ; 
       rotate(this.body.angle); 
       pop();
