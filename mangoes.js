f1c163944265bac94102b4bccc3ae74b28b5fd79
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-07 13:57:22 +0530

    Update mangoes.js

--- mangoes.js
+++ mangoes.js
@@ -1,6 +1,11 @@
 class fruit{
     constructor(x, y, width, height) {
-        this.body = Bodies.rectangle(x, y, width, height);
+        var op={
+        isStatic:true,
+        restitution:0,
+        friction:1
+      }
+        this.body = Bodies.rectangle(x, y, width, height,op);
         this.image = loadImage("mango.png");
         World.add(world, this.body);
 
