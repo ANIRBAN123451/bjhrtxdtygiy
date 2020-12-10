da94fb308fcc5664ad7ace3514f32608ce84468f
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-10 14:03:11 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -3,9 +3,8 @@ class rope{
         var options = {
             bodyA: bodyA,
             pointB: point2,
-            stiffness: 0.07,
-            length: 20
-            
+            stiffness: 0.04,
+            length: 10
         }
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -23,7 +22,7 @@ class rope{
             var pointB = this.posB;
             strokeWeight(3);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+      }
     }
     
 }
